66bc21d2208426041f2d62e1ef6850127a2cc5a9
Matías Surdi <matiassurdi@gmail.com>
2025-10-10 21:57:21 +0200

    Remove the abandoned fluent-ffmpeg package

--- app/services/ffmpeg.js
+++ app/services/ffmpeg.js
@@ -1,5 +1,4 @@
-const fluentFFmpeg = require("fluent-ffmpeg");
-const { execSync } = require("child_process");
+const { execSync, spawn } = require("child_process");
 const logger = require("./logger");
 
 // Check if ffmpeg is available in system PATH
@@ -20,55 +19,90 @@ const checkFFmpegAvailability = () => {
 checkFFmpegAvailability();
 
 class EncodingError extends Error {
-  constructor(stdout, stderr) {
-    super(`${stdout}\n${stderr}`);
+  constructor(message, stdout = "", stderr = "") {
+    super(message);
+    this.stdout = stdout;
+    this.stderr = stderr;
   }
 }
 
-const asPromise = (command) =>
-  new Promise((resolve, reject) => {
-    command
-      .on("start", (commandLine) => {
-        logger.debug(`Running ffmpeg command ${commandLine}`);
-      })
-      .on("error", (_err, stdout, stderr) => {
-        reject(new EncodingError(stdout, stderr));
-      })
-      .on("end", (stdout, stderr) => {
+const buildFFmpegArgs = (sourcePath, targetPath, { preview }) => {
+  const args = [
+    "-i",
+    sourcePath,
+    "-vf",
+    "scale='min(1280,iw)':'-2'",
+    "-f",
+    "mp4",
+    "-c:a",
+    "aac",
+    "-c:v",
+    "libsvtav1",
+    "-crf",
+    "30",
+    "-preset",
+    "6",
+    "-b:a",
+    "128k",
+    "-movflags",
+    "+faststart",
+    "-map",
+    "0",
+  ];
+
+  if (preview) {
+    args.splice(2, 0, "-t", "10"); // Insert duration after input file
+  }
+
+  args.push(targetPath);
+  return args;
+};
+
+const runFFmpegCommand = (args) => {
+  return new Promise((resolve, reject) => {
+    const command = spawn("ffmpeg", args);
+    let stdout = "";
+    let stderr = "";
+
+    command.stdout.on("data", (data) => {
+      stdout += data.toString();
+    });
+
+    command.stderr.on("data", (data) => {
+      stderr += data.toString();
+    });
+
+    command.on("spawn", () => {
+      logger.debug(`Running ffmpeg command: ffmpeg ${args.join(" ")}`);
+    });
+
+    command.on("error", (error) => {
+      reject(
+        new EncodingError(
+          `Failed to start ffmpeg: ${error.message}`,
+          stdout,
+          stderr
+        )
+      );
+    });
+
+    command.on("close", (code) => {
+      if (code === 0) {
         logger.debug(stdout);
         logger.debug(stderr);
         resolve();
-      });
+      } else {
+        reject(
+          new EncodingError(`ffmpeg exited with code ${code}`, stdout, stderr)
+        );
+      }
+    });
   });
-
-const withMp4Params = (command) => {
-  command.format("mp4").audioCodec("aac");
-  command.videoCodec("libsvtav1");
-
-  command.addOutputOptions([
-    "-crf 30",
-    "-preset 6",
-    "-b:a 128k",
-    "-movflags +faststart",
-    "-map 0",
-  ]);
-  return command;
 };
 
 const runFFmpeg = async (sourcePath, targetPath, { preview }) => {
-  const getBaseCommand = () => {
-    const command = fluentFFmpeg(sourcePath).videoFilter(
-      "scale='min(1280,iw)':'-2'"
-    );
-
-    if (preview) {
-      command.duration(10);
-    }
-
-    return command;
-  };
-
-  return asPromise(withMp4Params(getBaseCommand()).save(targetPath));
+  const args = buildFFmpegArgs(sourcePath, targetPath, { preview });
+  return runFFmpegCommand(args);
 };
 
 module.exports = { runFFmpeg, EncodingError };
